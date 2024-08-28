216e5c9fb6278207cb01d73c2aadcd4b3ca93d23
Farai Zihove <farai.zihove@econet.co.zw>
2024-08-28 15:20:27 +0200

    redirect to login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::redirect('/', '/backend');
+Route::redirect('login', '/backend');
+
+Auth::routes(['register' => false, 'login' => false]);
